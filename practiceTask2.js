275b05f0e29c0b0c3a16b0537d02943bdca4c03b
alfaysalabir <alfaysalabir@gmail.com>
2026-07-25 12:14:17 +0600

    4. Take a variable stock. If stock is 0, print Out of stock (use only if, no else).

--- practiceTask2.js
+++ practiceTask2.js
@@ -30,3 +30,10 @@ if (isRaining) {
 }
 
 console.log("===================")
+
+// Take a variable stock. If stock is 0, print "Out of stock" (use only if, no else).
+const stock = 0;
+
+if (stock === 0) {
+    console.log("Out of stock");
+}
