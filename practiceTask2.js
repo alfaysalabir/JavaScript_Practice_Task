3fc95e7adc21c364e7cdff796a1076de1056a5bb
alfaysalabir <alfaysalabir@gmail.com>
2026-07-25 13:00:46 +0600

    9. Using isWeekend and isHoliday, print No work today if either one is true (use ||).

--- practiceTask2.js
+++ practiceTask2.js
@@ -85,3 +85,11 @@ if (age > 18 && hasTicket) {
 }
 
 console.log("===================")
+
+// Using isWeekend and isHoliday, print "No work today" if either one is true (use ||).
+const isWeekend = false;
+const isHoliday = true;
+
+if (isWeekend || isHoliday) {
+    console.log("No work today");
+}
