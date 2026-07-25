25802cb0e04c4c7e94f4896bcb21b0e42cfc61f8
alfaysalabir <alfaysalabir@gmail.com>
2026-07-25 12:06:16 +0600

    3. Create a variable isRaining (true/false). If it is true, print Take an umbrella.

--- practiceTask2.js
+++ practiceTask2.js
@@ -19,3 +19,14 @@ console.log("===================")
 // Guess the result of "5" == 5 and "5" === 5 first, then write code to verify your guess.
 console.log("5" == 5);
 console.log("5" === 5);
+
+console.log("===================")
+
+// Create a variable isRaining (true/false). If it is true, print "Take an umbrella".
+const isRaining = true;
+
+if (isRaining) {
+    console.log("Take an umbrella");
+}
+
+console.log("===================")
