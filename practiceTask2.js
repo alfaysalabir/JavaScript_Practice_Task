e0ca883e9db997dd8528771cf2596e87fb83c314
alfaysalabir <alfaysalabir@gmail.com>
2026-07-25 11:49:28 +0600

    1. Take two variables price1 and price2, and compare them using all comparison operators (==, ===, !=, !==, >, <, >=, <=), printing each result with console.log.

--- practiceTask2.js
+++ practiceTask2.js
@@ -1,3 +1,15 @@
 /* ********* Module 8: Conditionals & Decision Making — Practice Tasks ********* */
 
 // Take two variables price1 and price2, and compare them using all comparison operators (==, ===, !=, !==, >, <, >=, <=), printing each result with console.log.
+const price1 = 300;
+const price2 = 500;
+
+console.log(price1 == price2);
+console.log(price1 === price2);
+console.log(price1 != price2);
+console.log(price1 == price2);
+console.log(price1 !== price2);
+console.log(price1 > price2);
+console.log(price1 < price2);
+console.log(price1 >= price2);
+console.log(price1 <= price2);
