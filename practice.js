2977c0fb39d6540c44b562df432c28ed4d4a78cd
alfaysalabir <alfaysalabir@gmail.com>
2026-07-26 17:40:58 +0600

    Practice JavaScript For Loop Table.

--- practice.js
+++ practice.js
@@ -275,5 +275,5 @@ console.log("Count Down End");
 const tableNum = 3;
 
 for (let n = 1; n <= 10; n++) {
-    console.log(n, "X", tableNum, " = ", n * tableNum);
+    console.log(tableNum, "X", n, " = ", n * tableNum);
 }
