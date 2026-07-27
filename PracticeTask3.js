e58231144f3d748449ca49c30db17db72ef1c1bb
alfaysalabir <alfaysalabir@gmail.com>
2026-07-27 11:31:35 +0600

    Write a for loop that prints Hello 5 times.

---
+++ PracticeTask3.js
@@ -0,0 +1,7 @@
+/* ********* Module 9: Fundamental Concepts of Loops ********* */
+
+// Task 1 — Print "Hello" Five Times
+// Write a for loop that prints "Hello" 5 times.
+for (let i = 1; i <= 5; i++) {
+    console.log("Hello");
+}
