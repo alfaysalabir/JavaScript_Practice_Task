aa1ca4821c88491b78555d6c89fd43a717c87937
alfaysalabir <alfaysalabir@gmail.com>
2026-07-27 11:37:31 +0600

    Write a for loop that prints numbers from 1 to 10.

--- PracticeTask3.js
+++ PracticeTask3.js
@@ -5,3 +5,11 @@
 for (let i = 1; i <= 5; i++) {
     console.log("Hello");
 }
+
+console.log("===================")
+
+// Task 2 — Print Numbers 1 to 10
+// Write a for loop that prints numbers from 1 to 10.
+for (let n = 1; n <= 10; n++) {
+    console.log(n);
+}
