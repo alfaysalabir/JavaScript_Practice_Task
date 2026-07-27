e6f5e8552480040a81eb80ea9597dbed3e1e658c
alfaysalabir <alfaysalabir@gmail.com>
2026-07-27 18:59:46 +0600

    Task 14 — Mini Challenge Write a program that:
    Loops from 1 to 100.
    Skips (continue) any number divisible by 5.
    Stops (break) completely once it reaches a number greater than 40.
    Prints every number that passes both checks.

--- PracticeTask3.js
+++ PracticeTask3.js
@@ -140,3 +140,22 @@ do {
     d++
 }
 while (d <= 5);
+
+console.log("===================")
+
+// Task 14 — Mini Challenge
+// Write a program that:
+
+// Loops from 1 to 100.
+// Skips(continue) any number divisible by 5.
+// Stops(break) completely once it reaches a number greater than 40.
+// Prints every number that passes both checks.
+for (let m = 1; m <= 100; m++) {
+    if (m > 40) {
+        break;
+    }
+    if (m % 5 === 0) {
+        continue
+    }
+    console.log(m);
+}
